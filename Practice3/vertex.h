71eab8d60a118af891fe1ed9d3171abdf78478f6
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-01 16:31:04 -0600

    Update vertex.h

--- Practice3/vertex.h
+++ Practice3/vertex.h
@@ -1,6 +1,15 @@
 #ifndef _VERTEX_H_
 #define _VERTEX_H_
 
-
+double **createZBuffer ();
+void VLF (double alfa, double beta, double gamma, double sfX, double sfY,
+	  double tX, double tY, char *name, char *rawFile);
+int readLines (char *rawFile);
+void vertexList (struct vertex *vertexes, int n, char *rawFile);
+void vertexList2 (struct vertex *vertexes, int n, char *rawFile);
+void edgesList (struct edge *edges, struct vertex *vertexes, int n,
+		char *rawFile);
+void facesList (struct face **faces, struct vertex *vertexes,
+		struct edge *edges, int n, char *rawFile);
 
 #endif
