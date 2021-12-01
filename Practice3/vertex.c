bf569f25c0d1b53cef6141c75885a62ab10a1930
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-01 15:12:15 -0600

    Create vertex.c

---
+++ Practice3/vertex.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+int j = 0;			
+int m = 0;			
+int fa = 0;			
+int vlc = 0;			
+
+
+void
+VLF ()
+{
+
+}
+
+/*
+  This function creates the ZBuffer matrix initialized with
+  bigger negative values (-10000 in this case).
+*/
+double **
+createZBuffer ()
+{
+  
+}
+
+/*
+  This function read how many lines does the raw file have.
+*/
+int
+readLines ()
+{
+  
+}
+
+/*
+  This function gets the vertex list using the raw file.
+*/
+void
+vertexList ()
+{
+  
+}
+
+/*
+  This function gets the auxiliar vertex list using the raw file.
+*/
+void
+vertexList2 ()
+{
+ 
+}
+
+/*
+  This function gets the edges list using the raw file.
+*/
+void
+edgesList ()
+{
+  
+}
+
+/*
+  This function gets the faces list using the raw file.
+*/
+void
+facesList ()
+{
+  
+}
