5353fb374ae016890e1d1cfac0267f55df18531d
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-01 14:45:07 -0600

    Update main.c

--- Practice3/main.c
+++ Practice3/main.c
@@ -5,5 +5,23 @@
 
 int main ()
 {
-
+ if ()
+    {
+    
+    }
+  else
+    {
+      printf ("Arguments are not enough \n");
+      printf ("(Or maybe you sent more)\n");
+      printf ("Try with the following pattern:\n");
+      printf ("./a.out alfa beta gamma sfX sfY tX tY outputName inputName\n");
+      printf ("Where:\n");
+      printf ("alfa\t: Rotation angle in the X axis\n");
+      printf ("beta\t: Rotation angle in the Y axis\n");
+      printf ("gamma\t: Rotation angle in the Z axis\n");
+      printf ("sfX\t: X axis scale factor\n");
+      printf ("sfY\t: Y axis scale factor\n");
+      printf ("tX\t: Translation in the X axis\n");
+      printf ("tY\t: Translation in the Y axis\n");
+    }
 }
