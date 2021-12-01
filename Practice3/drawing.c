268769eee2a6c34de37c2e996d22571e147b4c66
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-01 15:17:40 -0600

    Create drawing.c

---
+++ Practice3/drawing.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+
+double diffuseLight[3];
+struct vertex L;
+double cosineTheta, magnitude, dotProduct;
+
+
+/*
+  This function determines if a face is gonna be drawn or not.
+*/
+double
+faceHidding ()
+{
+  
+}
+
+/*
+  This fucntion fills the emply pixels of a face.
+*/
+void
+scanline ()
+{
+  
+}
