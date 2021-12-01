3702287409302955d6d52ef0bbf3d4473bcec786
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-01 16:33:37 -0600

    Update drawing.h

--- Practice3/drawing.h
+++ Practice3/drawing.h
@@ -3,5 +3,9 @@
 
 #define PI 3.14159265
 
+double faceHidding (struct face f, struct vertex *vertexes,
+		    struct edge *edges);
+void scanline (struct pixels ***Raster, double **ZBuffer, unsigned char *rgb,
+	       struct vertex normal);
 
 #endif
