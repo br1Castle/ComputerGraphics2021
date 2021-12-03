2c88356c403b7cbe57307215fd30a0a5661effbb
br1Castle <77824089+br1Castle@users.noreply.github.com>
2021-12-03 17:54:29 -0600

    completing

--- Practice3/main.c
+++ Practice3/main.c
@@ -5,11 +5,19 @@
 #include"bresenham.h"
 #include"vertex.h"
 
-int main ()
+int main (int argc, char *argv[])
 {
- if ()
+ if (argc == 10)
     {
-    
+      double alfa = atof (argv[1]);
+      double beta = atof (argv[2]);
+      double gamma = atof (argv[3]);
+      double sfX = atof (argv[4]);
+      double sfY = atof (argv[5]);
+      double tX = atof (argv[6]);
+      double tY = atof (argv[7]);
+      VLF (alfa, beta, gamma, sfX, sfY, tX, tY, argv[8], argv[9]);	
+      return 0;
     }
   else
     {
@@ -25,5 +33,9 @@ int main ()
       printf ("sfY\t: Y axis scale factor\n");
       printf ("tX\t: Translation in the X axis\n");
       printf ("tY\t: Translation in the Y axis\n");
+      printf ("outputName\t: The name your ppm image is going to have (Just the name, extension its not necessary)\n");
+      printf ("inputName\t: The raw file name  (Add the extension Ej.: cube.raw )\n");
+      printf ("An example could be:\n");
+      printf ("./a.out -0.5 0.1 0.2 1200 1200 960 540 myppm cube.raw\n");
     }
 }
